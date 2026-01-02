f69f58d2f33d91e8a4cd4049a6c2f5be6434b45c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2026-01-02 17:36:41 +0900

    feature: exclude api route in i18n process

--- init.ts
+++ init.ts
@@ -1,4 +1,7 @@
 import { createI18n } from '@inlang/paraglide-sveltekit';
 import * as runtime from '$i18n/output/runtime';
+import type { AvailableLanguageTags } from '$lib/utilities/language';
 
-export const i18n = createI18n(runtime);
+export const i18n = createI18n<AvailableLanguageTags>(runtime, {
+	exclude: ['/api/*']
+});
